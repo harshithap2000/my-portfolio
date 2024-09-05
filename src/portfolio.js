8b03694e333d13066527f2423f7b466744f00a7b
Harshitha <a1898543@adelaide.edu.au>
2024-09-05 11:38:53 +0930

    Initial Commit

---
+++ src/portfolio.js
@@ -0,0 +1,65 @@
+const header = {
+    homepage: 'https://harshithap2000.github.io/my-portfolio',  
+    title: 'HP.',
+  }
+  
+  const about = {
+    name: 'Harshitha Prakash',
+    role: 'Software Engineer',
+    description:
+      'Aspiring software engineer with experience in machine learning, big data, and software development. Specializes in creating efficient solutions and solving complex problems.',
+    resume: '/Harshitha Prakash Resume.pdf',  
+    social: {
+      linkedin: 'https://linkedin.com/in/harshitha-p22',
+      github: 'https://github.com/harshithap2000',
+    },
+  }
+  
+  const projects = [
+    {
+      name: 'Petri Net Modelling of Railway Networks',
+      description:
+        'Engineered a comprehensive Petri Net model using Java and JUnit to manage concurrent events in a complex rail yard system.',
+      stack: ['Java', 'JUnit'],
+      sourceCode: 'https://github.com/harshithap2000/Petri-net-Modelling-for-Railway-Networks',
+      livePreview: '',  
+    },
+    {
+      name: 'Obesity Prediction using PySpark',
+      description:
+        'Built regression and classification models using PySpark to predict obesity based on health indicators for 10,000 individuals.',
+      stack: ['PySpark', 'Python', 'Machine Learning'],
+      sourceCode: 'https://github.com/harshithap2000/Obesity-Prediction',
+      livePreview: '',  
+    },
+    {
+      name: 'Text Classification and Sentiment Analysis',
+      description:
+        'Developed a sentiment analysis model using NLP techniques and machine learning algorithms to classify hotel reviews with 90.5% accuracy.',
+      stack: ['Python', 'NLP', 'Scikit-learn'],
+      sourceCode: 'https://github.com/harshithap2000/Text-Classification-and-Sentiment-Analysis-of-Hotel-Reviews',
+      livePreview: '', 
+    },
+  ]
+  
+  const skills = [
+    'C',
+    'C++',
+    'Java',
+    'Python',
+    'Javascript',
+    'HTML',
+    'CSS',
+    'Android Studio',
+    'React',
+    'APIs',
+    'AI/ML',
+    'Github',
+    'AWS',
+  ]
+  
+  const contact = {
+    email: 'harshitha.prakash22@gmail.com',  
+  }
+  
+  export { header, about, projects, skills, contact }  
